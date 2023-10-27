aa3c46ddbb44b0c56231a2abbd5bb5da164e6408
mattbro369 <ethacktry@gmail.com>
2023-10-27 15:18:48 +0200

    Stopped sidenav content reprinting on reopening of nav

--- src/sidenav.js
+++ src/sidenav.js
@@ -3,79 +3,59 @@ import { sideNavBar } from '.';
 
 export const sideNavFunc = (function () {
   let isSideNavOpen = false;
+  let sideNavRendered = false;
+
+  const renderSideNav = function () {
+    sideNavBar.style.width = '250px';
+    mainContent.style.marginLeft = '250px';
+
+    const sidebarSectionName = [
+      '\u2715',
+      'About',
+      'Services',
+      'Client',
+      'Contact',
+    ];
+
+    for (let i = 0; i < 5; i++) {
+      let element = document.createElement('a');
+
+      if (i === 0) {
+        element.classList.add('closebutton');
+        element.addEventListener('click', () => {
+          sideNavFunc.close();
+        });
+        element.textContent = sidebarSectionName[0];
+      } else {
+        element.setAttribute('href', '#');
+        element.textContent = sidebarSectionName[i];
+      }
+      element.classList.add('sidenav-item');
+      sideNavBar.appendChild(element);
+    }
+  };
 
   const open = function () {
-    if (!isSideNavOpen) {
+    if (!isSideNavOpen && !sideNavRendered) {
+      renderSideNav();
+    } else {
       sideNavBar.style.width = '250px';
       mainContent.style.marginLeft = '250px';
-
-      const sidebarSectionName = [
-        '\u2715',
-        'About',
-        'Services',
-        'Client',
-        'Contact',
-      ];
-
-      for (let i = 0; i < 5; i++) {
-        let element = document.createElement('a');
-
-        if (i === 0) {
-          element.classList.add('closebutton');
-          element.addEventListener('click', () => {
-            console.log('click');
-            sideNavFunc.close();
-          });
-          element.textContent = sidebarSectionName[0];
-        } else {
-          element.setAttribute('href', '#');
-          element.textContent = sidebarSectionName[i];
-        }
-
-        sideNavBar.appendChild(element);
-      }
     }
     isSideNavOpen = true;
+    sideNavRendered = true;
   };
 
   const close = function () {
     if (isSideNavOpen) {
       // Check if the sidebar is open
+      sideNavBar.style.transition = '0.5s';
       sideNavBar.style.width = '0';
       mainContent.style.marginLeft = '0';
-      sideNavBar.innerHTML = ''; // Clear the sidebar content
+      // sideNavBar.innerHTML = '';
       isSideNavOpen = false; // Set the sidebar state to closed
     }
   };
 
-  return { open, close, isSideNavOpen };
+  return { renderSideNav, open, close, isSideNavOpen };
 })();
-
-// export function openSideNav() {
-//   let isSideNavOpen = true;
-
-//   sideNavBar.style.width = '250px';
-//   mainContent.style.marginLeft = '250px';
-
-//   const sidebarSectionName = [
-//     '\u2715',
-//     'About',
-//     'Services',
-//     'Client',
-//     'Contact',
-//   ];
-
-//   for (let i = 0; i < 5; i++) {
-//     const element = document.createElement('a');
-//     if (i === 0) {
-//       element.setAttribute('href', 'javascript:void(0)');
-//       element.classList.add('closebutton');
-//       element.textContent = sidebarSectionName[0];
-//     } else {
-//       element.setAttribute('href', '#');
-//       element.textContent = sidebarSectionName[i];
-//     }
-
-//     sideNavBar.appendChild(element);
-//   }
-// }
