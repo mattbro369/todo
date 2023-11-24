7a2afa8efa36b6bd7d62c108990e5920c98a3575
mattbro369 <ethacktry@gmail.com>
2023-11-24 18:51:16 +0100

    added basic functions to tasks

--- src/tasks.js
+++ src/tasks.js
@@ -1,9 +1,38 @@
+import { renderMain } from './content';
+import addIconImg from './assets/add.svg';
+import { contentWrapper, taskArray } from '.';
+
 export const task = (function () {
+  // Add Task Button
+  const renderAddTaskButton = function (target) {
+    let addTaskButton = document.createElement('button');
+    let addIcon = document.createElement('img');
+    let addButtonText = document.createElement('div');
+
+    addIcon.src = addIconImg;
+    addIcon.id = 'add-task-icon';
+    addButtonText.textContent = 'Add task';
+    addTaskButton.id = 'add-task-button';
+    addTaskButton.appendChild(addIcon);
+    addTaskButton.appendChild(addButtonText);
+    target.appendChild(addTaskButton);
+
+    addTaskButton.addEventListener('click', () => {
+      removeAddTaskButton();
+      renderAddTaskPrompt(contentWrapper);
+    });
+  };
+
   const renderAddTaskPrompt = function (target) {
     let addTaskWrapper = document.createElement('div');
     addTaskWrapper.classList.add('add-task-wrapper');
     target.appendChild(addTaskWrapper);
   };
 
-  return { renderAddTaskPrompt };
+  const removeAddTaskButton = function () {
+    const target = document.getElementById('add-task-button');
+    target.remove();
+  };
+
+  return { renderAddTaskPrompt, renderAddTaskButton };
 })();
