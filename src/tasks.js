0b932a0f76aceeb1a5cc93bd53ff5458964d6dfe
mattbro369 <ethacktry@gmail.com>
2023-11-24 16:23:31 +0100

    added parameter of target to load add task prompt

--- src/tasks.js
+++ src/tasks.js
@@ -1,5 +1,9 @@
-import mainContent from '.';
-
 export const task = (function () {
-  const renderAddTaskPrompt = function () {};
+  const renderAddTaskPrompt = function (target) {
+    let addTaskWrapper = document.createElement('div');
+    addTaskWrapper.classList.add('add-task-wrapper');
+    target.appendChild(addTaskWrapper);
+  };
+
+  return { renderAddTaskPrompt };
 })();
